d5f239e80f1927f849158642b0a4e110a59021fd
Anano Topuria <anano.topuriaa@gmail.com>
2023-10-12 17:45:07 +0400

    use css modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom"
+import PageNav from "../components/PageNav"
+import AppNav from "../components/AppNav"
+
 function Homepage() {
     return (
         <div>
-            WorldWise
+            <PageNav />
+            <AppNav />
+            <h1>WorldWise</h1>
+            <Link to="/app">go to the app</Link>
         </div>
     )
 }
